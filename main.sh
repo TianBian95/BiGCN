515abfe924c91cfbb44b70703746a5ce40a4808a
TianBian <bt18@mails.tsinghua.edu.cn>
2020-02-11 14:26:27 +0800

    add "unzip weibotree.txt.zip"

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+unzip -d ./data/Weibo ./data/Weibo/weibotree.txt.zip
 virtualenv venv
 source venv/bin/activate
 pip install torch==1.4.0
@@ -10,7 +11,7 @@ python ./Process/getTwittergraph.py Twitter15
 #Generate graph data and store in /data/Twitter16graph
 python ./Process/getTwittergraph.py Twitter16
 #Reproduce the experimental results.
-python ./model/Weibo/BiGCN_Weibo.py 100
-#python ./model/Twitter/BiGCN_Twitter.py Twitter15 100
+#python ./model/Weibo/BiGCN_Weibo.py 100
+python ./model/Twitter/BiGCN_Twitter.py Twitter15 10
 #python ./model/Twitter/BiGCN_Twitter.py Twitter16 100
 deactivate
